d287de95899c1c664bc4e7ca2684be46d1bff474
Hayley Witherell <hayleywitherell@gmail.com>
2021-10-16 16:44:38 -0600

    Add store images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,10 @@ import facebookLogo from '../../assets/social/facebook-white.svg';
 import twitterLogo from '../../assets/social/twitter-white.svg';
 import instagramLogo from '../../assets/social/instagram-white.svg';
 
+import appStore from '../../assets/store/app-store.svg';
+import playStore from '../../assets/store/play-store.svg';
+import windowsStore from '../../assets/store/windows-store.svg';
+
 class Footer extends Component {
 
   render() {
@@ -20,23 +24,49 @@ class Footer extends Component {
 
         <section className='icon-container'>
 
-          <img
-            src={facebookLogo}
-            className='social-icon'
-            alt='Facebook logo'
-          />
+          <section className='social-icons'>
 
-          <img
-            src={twitterLogo}
-            className='social-icon'
-            alt='Twitter logo'
-          />
+            <img
+              src={facebookLogo}
+              className='social-icon'
+              alt='Facebook logo'
+            />
 
-          <img
-            src={instagramLogo}
-            className='social-icon'
-            alt='Instagram logo'
-          />                  
+            <img
+              src={twitterLogo}
+              className='social-icon'
+              alt='Twitter logo'
+            />
+
+            <img
+              src={instagramLogo}
+              className='social-icon'
+              alt='Instagram logo'
+            />                  
+
+          </section>
+
+          <section className='store-icons'>
+
+            <img
+              src={appStore}
+              className='store-icon'
+              alt='App Store logo'
+            />        
+
+            <img
+              src={playStore}
+              className='store-icon'
+              alt='Play Store logo'
+            />     
+
+            <img
+              src={windowsStore}
+              className='store-icon'
+              alt='Windows Store logo'
+            />                            
+
+          </section>
 
         </section>
 
