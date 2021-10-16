9d04d5d3a725b3e40c228d511a5c28da4478218d
Hayley Witherell <hayleywitherell@gmail.com>
2021-10-16 12:25:56 -0600

    Create App.js skeleton

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Auddia Test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
