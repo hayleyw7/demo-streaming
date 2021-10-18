b4d7e18ec55ce9079c594bc0617a70e2aa85235d
Hayley Witherell <hayleywitherell@gmail.com>
2021-10-17 19:33:49 -0600

    Add header test

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -7,4 +7,22 @@ describe('Home', () => {
   it('should keep original url', () => {
     cy.url().should('eq', 'http://localhost:3000/')
   })
+
+  it('should render header', () => {
+    cy.get('h1')
+      .contains('DEMO')
+    .get('button')
+      .contains('Log')
+    .get('button')
+      .contains('trial')
+  })
+
+  it('should render cards', () => {
+    cy.get('h1')
+      .contains('DEMO')
+    .get('button')
+      .contains('Log')
+    .get('button')
+      .contains('trial')
+  })
 })
